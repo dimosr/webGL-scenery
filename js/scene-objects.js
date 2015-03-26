d6ec9bc37782cfb4fd143bb22fd725156de61cba
Dimos Raptis <dr@eworx.gr>
2015-03-26 19:17:59 +0200

    improved scenery textures

--- js/scene-objects.js
+++ js/scene-objects.js
@@ -1,6 +1,8 @@
 function createGroundSurface(dimensionX, dimensionY, textureIMG, anisotropy){
 	var geometry = new THREE.PlaneGeometry( dimensionX, dimensionY);
-	var texture = THREE.ImageUtils.loadTexture( textureIMG );
+	var texture = new THREE.ImageUtils.loadTexture( textureIMG );
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(50,50);
 	texture.anisotropy = anisotropy;
 	var material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
 	var plane = new THREE.Mesh( geometry, material );
@@ -9,14 +11,16 @@ function createGroundSurface(dimensionX, dimensionY, textureIMG, anisotropy){
 
 function createSkySphere(radius, textureIMG){
 	var geometry = new THREE.SphereGeometry(radius);
-	var texture = THREE.ImageUtils.loadTexture( textureIMG );
+	var texture = new THREE.ImageUtils.loadTexture( textureIMG );
+	//texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    //texture.repeat.set(2,2);
 	var material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
 	var sphere = new THREE.Mesh( geometry, material );
 	return sphere;
 }
 
 function createSceneObjects(sceneObject){
-	var ground = createGroundSurface(2000, 2000, 'textures/grass3.jpg', renderer.getMaxAnisotropy());
+	var ground = createGroundSurface(2000, 2000, 'textures/grass.png', renderer.getMaxAnisotropy());
 	scene.xBoundary = (0.92*2000)/2;
 	scene.yBoundary = (0.92*2000)/2;
 	scene.add(ground);
